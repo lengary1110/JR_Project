900552c4e9f5eeb3a25349a871b94b4157d3d7f8
lengary1110 <lengary1110@xuziyuandeMacBook-Pro.local>
2021-01-09 13:17:56 +0800

    add P1

---
+++ P1/js/index.js
@@ -0,0 +1,32 @@
+let previous = 'BLOG';
+let current = 'BLOG';
+
+document.querySelectorAll('.navItem')
+        .forEach(function(element) {
+          element.onclick = function(event) {
+            event.preventDefault();
+
+            let next = event.target.getAttribute('href');
+            push(next);
+          };
+        });
+
+
+const push = (next) => {
+  previous = current;
+  current = next;
+  changePage();
+}
+
+const changePage = () => {
+  let className = 'page--active';
+
+  if (previous) {
+    let previousPage = document.querySelector('#' + previous);
+    previousPage.classList.remove(className);
+  }
+
+  let page = document.querySelector('#' + current);
+  page.classList.add(className);  
+}
+
